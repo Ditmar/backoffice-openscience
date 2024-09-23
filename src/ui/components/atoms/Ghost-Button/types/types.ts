02f0adfe9cd4def941586df0d105e2a338b5988c
kymer591 <albaroJhaz@gmail.com>
2024-09-23 04:42:07 -0400

    feat:SEM-10 Ghostbutton

---
+++ src/ui/components/atoms/Ghost-Button/types/types.ts
@@ -0,0 +1,8 @@
+export interface GhostButtonProps {
+    ghostbutton: string;
+    variant: 'primary' | 'secondary';
+    size?: 'small' | 'medium' | 'large';
+    disabled?: boolean;
+    onClick?: () => void;
+  }
+  
