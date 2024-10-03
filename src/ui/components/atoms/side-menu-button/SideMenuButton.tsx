503c5adefd580898ae97ac4a499a89e966f55044
Then-Crimson <miaut16umf@gmail.com>
2024-10-03 00:56:42 -0400

    modifying the colors of my component and adding a new variable

--- src/ui/components/atoms/side-menu-button/SideMenuButton.tsx
+++ src/ui/components/atoms/side-menu-button/SideMenuButton.tsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import type { IProps } from './types/IProps';
 import styles from './styles.module.scss';
 
-function SideMenuButton({ children, icon, color, variant }: IProps) {
-  const buttonClasses = `${styles['side-menu-button']} ${styles[color ?? '']} ${
-    variant ? styles[variant] : ''
-  }`;
+const SideMenuButton: React.FC<IProps> = function SideMenuButton({
+  children,
+  icon,
+  color,
+  variant,
+}) {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = () => {
+    setIsActive(!isActive);
+  };
 
   return (
-    <button data-testid="side-menu-button-styles" className={buttonClasses}>
+    <button
+      data-testid="side-menu-button-styles"
+      className={`${styles['side-menu-button']} ${styles[color ?? '']} ${
+        variant ? styles[variant] : ''
+      } ${isActive ? styles.active : ''}`}
+      onClick={handleClick}
+    >
       {icon && <img src={icon} alt="Icon" />}
       {children}
     </button>
   );
-}
+};
 
 export default SideMenuButton;
