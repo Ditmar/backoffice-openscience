e94be85232fb2cf7dd6c10a5b2dc26513ce5cd4b
Elvis Rodrigo  Condori Vasquez <ec7902812@gmail.com>
2024-10-03 15:22:07 -0400

    feature/SEM-023

--- src/ui/atoms/TabLabel/TabLabel.stories.tsx
+++ src/ui/atoms/TabLabel/TabLabel.stories.tsx
@@ -1,49 +1,48 @@
-import type { StoryObj, Meta } from "@storybook/react"
+import type { StoryObj, Meta } from '@storybook/react';
 
-import { TabLabel } from "./TabLabel"
+import { TabLabel } from './TabLabel';
 
 const meta: Meta<typeof TabLabel> = {
-    title: "ui/atoms/TabLabel",
-    component: TabLabel,
-    
-    argTypes: {
-        text: {
-            control: "text",
-            type: { name: "string", required: true },
-        },
-        isEnable: {
-            control: "boolean",
-            type: { name: "boolean", required: true },
-        },
-    },
-}
+  title: 'ui/atoms/TabLabel',
+  component: TabLabel,
 
+  argTypes: {
+    text: {
+      control: 'text',
+      type: { name: 'string', required: true },
+    },
+    isEnable: {
+      control: 'boolean',
+      type: { name: 'boolean', required: true },
+    },
+  },
+};
 
 export default meta;
 
-type Story = StoryObj<typeof meta>
+type Story = StoryObj<typeof meta>;
 
 export const Small: Story = {
-    args: {
-        text: "TabLabel",
-        isEnable: false,
-        variant: "small",
-    },
-}
+  args: {
+    text: 'TabLabel',
+    isEnable: false,
+    variant: 'small',
+  },
+};
 
-export const medium: Story = {
-    args: {
-        text: "TabLabel",
-        isEnable: true,
-        variant: "medium",
-        isSelect: true,
-    },
-}
+export const Medium: Story = {
+  args: {
+    text: 'TabLabel',
+    isEnable: true,
+    variant: 'medium',
+    isSelect: true,
+  },
+};
 
 export const Large: Story = {
-    args: {
-        text: "TabLabel",
-        isEnable: true,
-        variant: "large",
-    },
-}
+  args: {
+    text: 'TabLabel',
+    isEnable: true,
+    variant: 'large',
+  },
+};
--- src/ui/atoms/TabLabel/TabLabel.tsx
+++ src/ui/atoms/TabLabel/TabLabel.tsx
@@ -1,20 +1,17 @@
-import type { IProps } from "./types/IProps"
-import styles from "./styles.module.scss"
-import classNames from "classnames";
+import classNames from 'classnames';
+import type { IProps } from './types/IProps';
+import styles from './styles.module.scss';
+
 export function TabLabel(props: IProps) {
-    const { text, variant, isEnable, isSelect } = props;
-    const setActive = isSelect || false;
-    const tabLabelClass = classNames(styles["tabLabel"], {
-        [styles['small']]: variant === 'small',
-        [styles['medium']]: variant === 'medium',
-        [styles['large']]: variant === 'large',
-        [styles['disabled']]: !isEnable,
-        [styles['active']]: isEnable,
-        [styles['selected']]: setActive
-    });
-    return (
-        <div className={tabLabelClass}>
-            {text}
-        </div >
-    )
+  const { text, variant, isEnable, isSelect } = props;
+  const setActive = isSelect ?? false;
+  const tabLabelClass = classNames(styles.tabLabel, {
+    [styles.small]: variant === 'small',
+    [styles.medium]: variant === 'medium',
+    [styles.large]: variant === 'large',
+    [styles.disabled]: !isEnable,
+    [styles.active]: isEnable,
+    [styles.selected]: setActive,
+  });
+  return <div className={tabLabelClass}>{text}</div>;
 }
