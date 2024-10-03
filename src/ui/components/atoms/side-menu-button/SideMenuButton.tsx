58478c810be26304919e406843f870d0c4186b25
Then-Crimson <miaut16umf@gmail.com>
2024-10-03 11:46:50 -0400

    modifying and adding necessary improvements

--- src/ui/components/atoms/side-menu-button/SideMenuButton.tsx
+++ src/ui/components/atoms/side-menu-button/SideMenuButton.tsx
@@ -14,6 +14,9 @@ function SideMenuButton({ children, icon, color, variant, onClick }: IProps) {
     if (onClick) {
       onClick();
     }
+    if (onClick) {
+      onClick();
+    }
   };
 
   const selectedIcon =
@@ -26,12 +29,14 @@ function SideMenuButton({ children, icon, color, variant, onClick }: IProps) {
     { [styles.active]: isActive },
   );
 
+  const selectedIcon = icon ?? (children === 'Article Management' ? Icon1 : Icon2);
+
   return (
     <button data-testid="side-menu-button-styles" className={buttonClass} onClick={handleClick}>
       {selectedIcon && <Icon src={selectedIcon} size="24px" className={styles.icon} />}
       {children}
     </button>
   );
-};
+}
 
 export default SideMenuButton;
