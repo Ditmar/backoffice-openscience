dea8469cf6db7a77dc91f80b6451672b31101e5e
Josse <cooper17john@gmail.com>
2024-10-03 12:36:20 -0400

    added atom tag component

--- src/ui/components/atoms/types/types.ts
+++ src/ui/components/atoms/types/types.ts
@@ -1,7 +1,7 @@
 export interface TagProps {
   label: string;
   size?: 'small' | 'medium' | 'large';
-  variant?: 'default' | 'outlined' | 'ghost';
+  variant?: 'default' | 'outlined' | 'ghost' | 'primary' | 'secondary' | 'tertiary';
   disabled?: boolean;
   onClick?: () => void;
 }
