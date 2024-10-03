7fde4d75174252bc1f6a6de08c7ef28b38cc5f04
Elvis Rodrigo  Condori Vasquez <ec7902812@gmail.com>
2024-10-03 15:22:07 -0400

    feature/SEM-023

--- src/ui/atoms/TabLabel/types/IProps.ts
+++ src/ui/atoms/TabLabel/types/IProps.ts
@@ -1,6 +1,6 @@
 export interface IProps {
-    text: string,
-    isEnable: boolean,
-    variant: 'small' | 'medium' | 'large',
-    isSelect?: boolean,
+  text: string;
+  isEnable: boolean;
+  variant: 'small' | 'medium' | 'large';
+  isSelect?: boolean;
 }
