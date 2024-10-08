8db10ca003c4d3b0b8df2716cef10c3582575df4
Beymar <patriciobeymar4@gmail.com>
2024-10-07 22:12:10 -0400

    feature fix:added button yes/no

--- src/ui/atoms/ButtonYesNo/Button.tsx
+++ src/ui/atoms/ButtonYesNo/Button.tsx
@@ -1,14 +1,18 @@
+import classNames from 'classnames';
 import type { IProps } from './types/IProps';
 import styles from './styles.module.scss';
 
-function Button({ children, variant = 'primary' }: IProps) {
-  const colorFont = styles[`button-label--${variant}`];
+function Button({ children, variant }: IProps) {
+  const buttonClass = classNames(styles['button-label--container'], {
+    [styles['button-label--primary']]: variant === 'primary',
+    [styles['button-label--secondary']]: variant === 'secondary',
+    [styles['button-label--tertiary']]: variant === 'tertiary',
+    [styles['button-label--quaternary']]: variant === 'quaternary',
+    [styles['button-label--quinary']]: variant === 'quinary',
+  });
 
   return (
-    <button
-      className={`${styles['button-label--container']} ${colorFont}`}
-      data-testid="button-styles"
-    >
+    <button className={buttonClass} data-testid="button-styles">
       <span>{children}</span>
     </button>
   );
