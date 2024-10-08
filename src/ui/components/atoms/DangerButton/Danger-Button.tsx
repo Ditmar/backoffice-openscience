4b7d9dc654478de764e554b1cfb9f87b4324dfde
nnina32 <noeminina18@gmail.com>
2024-10-08 17:18:30 -0400

    fix: danger button

--- src/ui/components/atoms/DangerButton/Danger-Button.tsx
+++ src/ui/components/atoms/DangerButton/Danger-Button.tsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames';
 import type { IProps } from './types/IProps';
-import styles from './Danger-Button.scss';
+import styles from './Danger-Button.module.scss';
 
 function DangerButton(props: IProps) {
   const { children, variant, onClick } = props;
@@ -15,9 +15,11 @@ function DangerButton(props: IProps) {
     [styles.disable]: isDisabled,
   });
   return (
-    <button className={displayClass}
+    <button
+      className={displayClass}
       onClick={isDisabled ? undefined : onClick}
-      disabled={isDisabled}>
+      disabled={isDisabled}
+    >
       <span className="variable-button__text">{children}</span>
     </button>
   );
