f5da76bae2346ca590993392f2f1a7fd9c648336
henrry <henrryademarmamani@gmail.com>
2024-10-08 16:00:22 -0400

    feat: SEM-022 add Links component with variants

--- src/ui/components/atoms/Links/types/IProps.ts
+++ src/ui/components/atoms/Links/types/IProps.ts
@@ -3,5 +3,5 @@ import React from 'react';
 export interface IProps {
   children?: React.ReactNode;
   onClick?: () => void;
-  variant?: 'Small' | 'Medium' | 'Big' | 'Active' | 'Disabled';
+  variant?: 'Enabled' | 'Disabled';
 }
