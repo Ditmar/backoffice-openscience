2f3f6d92a64f26a56a5dcedeb07542098d079e47
Then-Crimson <miaut16umf@gmail.com>
2024-10-08 21:14:29 -0400

    Fixing the classnames and improving the scss styles of my component

--- src/ui/components/atoms/side-menu-button/SideMenuButton.tsx
+++ src/ui/components/atoms/side-menu-button/SideMenuButton.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import classNames from 'classnames';
+import { Icon } from '../../utils/svg-icons/icons';
 import type { IProps } from './types/IProps';
 import styles from './styles.module.scss';
-import Icon1 from '../../../../assets/icons/article-management.svg?raw';
-import Icon2 from '../../../../assets/icons/author-management.svg?raw';
+import articleManagementSvg from '../../../../assets/icons/article-management.svg?raw';
+import authorManagementSvg from '../../../../assets/icons/author-management.svg?raw';
 
 function SideMenuButton({ children, icon, color, variant, onClick }: IProps) {
   const [isActive, setIsActive] = useState(false);
@@ -14,17 +16,19 @@ function SideMenuButton({ children, icon, color, variant, onClick }: IProps) {
     }
   };
 
-  const selectedIcon = icon ?? (children === 'Article Management' ? Icon1 : Icon2);
+  const selectedIcon =
+    icon ?? (children === 'Article Management' ? articleManagementSvg : authorManagementSvg);
+
+  const buttonClass = classNames(
+    styles['side-menu-button'],
+    color && styles[color],
+    variant && styles[variant],
+    { [styles.active]: isActive },
+  );
 
   return (
-    <button
-      data-testid="side-menu-button-styles"
-      className={`${styles['side-menu-button']} ${styles[color ?? '']} ${
-        variant ? styles[variant] : ''
-      } ${isActive ? styles.active : ''}`}
-      onClick={handleClick}
-    >
-      {selectedIcon && <img src={selectedIcon} alt="Icon" />}
+    <button data-testid="side-menu-button-styles" className={buttonClass} onClick={handleClick}>
+      {selectedIcon && <Icon src={selectedIcon} size="24px" className={styles.icon} />}
       {children}
     </button>
   );
