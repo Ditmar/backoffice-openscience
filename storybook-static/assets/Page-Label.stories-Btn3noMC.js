86e49344856a7be07fdbc0697c775f9546e55bb5
cinthiaastoraiqueh <cinthiaastoraique2@gmail.com>
2024-10-09 21:28:02 -0400

    Agrega Storybook estático

---
+++ storybook-static/assets/Page-Label.stories-Btn3noMC.js
@@ -0,0 +1,16 @@
+import{P as u}from"./Page-Label-BNnF-2vF.js";import"./jsx-runtime-DEdD30eg.js";import"./index-RYns6xqu.js";const f={title:"ui/components/atoms/PageIndicator",component:u,argTypes:{onClick:{action:"clicked"}}},a={args:{currentPage:1,totalPages:5}},e={args:{currentPage:3,totalPages:5}},r={args:{currentPage:5,totalPages:5}};var t,s,o;a.parameters={...a.parameters,docs:{...(t=a.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    currentPage: 1,
+    totalPages: 5
+  }
+}`,...(o=(s=a.parameters)==null?void 0:s.docs)==null?void 0:o.source}}};var n,c,g;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    currentPage: 3,
+    totalPages: 5
+  }
+}`,...(g=(c=e.parameters)==null?void 0:c.docs)==null?void 0:g.source}}};var m,p,d;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    currentPage: 5,
+    totalPages: 5
+  }
+}`,...(d=(p=r.parameters)==null?void 0:p.docs)==null?void 0:d.source}}};const S=["Default","MiddlePage","LastPage"];export{a as Default,r as LastPage,e as MiddlePage,S as __namedExportsOrder,f as default};
