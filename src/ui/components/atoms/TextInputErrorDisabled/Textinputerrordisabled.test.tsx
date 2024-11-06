886091b1b213129c5ddaf9123d88d74412587ffa
user.name <email@gmail.com>
2024-11-05 22:23:40 -0400

    commit - fix

--- src/ui/components/atoms/TextInputErrorDisabled/Textinputerrordisabled.test.tsx
+++ src/ui/components/atoms/TextInputErrorDisabled/Textinputerrordisabled.test.tsx
@@ -1,10 +1,7 @@
-// TextInputErrorDisabled.test.tsx
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import { describe, it, expect } from 'vitest';
 import '@testing-library/jest-dom';
-
 import TextInputErrorDisabled from './Textinputerrordisabled';
 
 describe('TextInputErrorDisabled Component', () => {
@@ -21,7 +18,7 @@ describe('TextInputErrorDisabled Component', () => {
   });
 
   it('disables the input when isDisabled is true', () => {
-    render(<TextInputErrorDisabled isDisabled={true} />);
+    render(<TextInputErrorDisabled isDisabled />);
     const inputElement = screen.getByRole('textbox');
     expect(inputElement).toBeDisabled();
   });
@@ -35,18 +32,14 @@ describe('TextInputErrorDisabled Component', () => {
   it('allows user to type when not disabled', async () => {
     render(<TextInputErrorDisabled isDisabled={false} placeholder="Type here" />);
     const inputElement = screen.getByPlaceholderText('Type here');
-    
-    await userEvent.type(inputElement, 'Test input');
-    
+    await MouseEvent.type(inputElement, 'Test input');
     expect(inputElement).toHaveValue('Test input');
   });
 
   it('does not allow user to type when disabled', async () => {
-    render(<TextInputErrorDisabled isDisabled={true} placeholder="Type here" />);
+    render(<TextInputErrorDisabled isDisabled placeholder="Type here" />);
     const inputElement = screen.getByPlaceholderText('Type here');
-
     await userEvent.type(inputElement, 'Test input');
-    
-    expect(inputElement).toHaveValue(''); // Should remain empty as input is disabled
+    expect(inputElement).toHaveValue('');
   });
 });
--- src/ui/components/atoms/TextInputErrorDisabled/types/IProps.ts
+++ src/ui/components/atoms/TextInputErrorDisabled/types/IProps.ts
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import type { InputHTMLAttributes } from 'react';
 
 export interface TextInputErrorDisabledProps extends InputHTMLAttributes<HTMLInputElement> {
