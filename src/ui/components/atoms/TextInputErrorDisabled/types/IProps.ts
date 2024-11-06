26ba37747f4a4b89085c618e26067dc15a436edf
user.name <email@gmail.com>
2024-11-06 17:14:22 -0400

    reload branch

--- src/ui/components/atoms/TextInputErrorDisabled/types/IProps.ts
+++
@@ -1,9 +0,0 @@
-import type { InputHTMLAttributes } from 'react';
-
-export interface TextInputErrorDisabledProps extends InputHTMLAttributes<HTMLInputElement> {
-  variant?: 'small' | 'medium' | 'large';
-  errorMessage?: string;
-  isDisabled?: boolean;
-  hasError?: boolean;
-  value?: string; // Asegúrate de que la propiedad value esté incluida si la necesitas
-}
