450adc7c63e755a9ddb3b7081849022696f94dab
user.name <email@gmail.com>
2024-11-07 19:30:06 -0400

    import @breakpoint, replace name:storyname -> build

---
+++ storybook-static/assets/MagnifyingGlass.stories-CrjcdclU.js
@@ -0,0 +1,6 @@
+import{M as e}from"./MagnifyingGlass-pQz5dxxj.js";import{s as t}from"./search-D-lBujjI.js";import"./jsx-runtime-DEdD30eg.js";import"./index-RYns6xqu.js";import"./icons-C45oln94.js";import"./index-rNTiGNI1.js";const g={title:"ui/components/atoms/MagnifyingGlass",component:e,argTypes:{onClick:{action:"clicked"}}},o={args:{onClick:()=>{},icon:t}};var r,a,s;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    onClick: () => {},
+    icon: search
+  }
+}`,...(s=(a=o.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};const d=["Default"];export{o as Default,d as __namedExportsOrder,g as default};
