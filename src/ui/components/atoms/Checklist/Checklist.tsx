f808d17d2839db1d4c8f3c0223f9238fcd51e96f
Elvis Rodrigo  Condori Vasquez <ec7902812@gmail.com>
2024-11-22 22:07:38 -0400

    feature/SEM-61

--- src/ui/components/atoms/Checklist/Checklist.tsx
+++ src/ui/components/atoms/Checklist/Checklist.tsx
@@ -3,6 +3,7 @@ import classNames from 'classnames';
 import type { IProps } from './types/IProps';
 import style from './styles.module.scss';
 
+
 function Checklist({ items, onCheck, onSeleonCategorySelect, className }: IProps) {
   const handleCheck = ({ target: { value } }: React.ChangeEvent<HTMLSelectElement>) => {
     onCheck(value);
