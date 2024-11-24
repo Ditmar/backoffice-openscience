3f27ddd7128ae390c3a0572ff1c7e8b5948a7743
user.name <email@gmail.com>
2024-11-24 14:07:18 -0400

    error version yarn -> commit revet

---
+++ src/ui/components/atoms/TextInputErrorDisabled/TextInputErrorDisabled.tsx
@@ -0,0 +1,32 @@
+import { IoAlertCircle } from 'react-icons/io5';
+import type { TextInputErrorDisabledProps } from './types/IProps';
+import styles from './TextInputErrorDisabled.module.scss';
+
+function TextInputErrorDisabled({
+  variant = 'medium',
+  errorMessage,
+  isDisabled,
+  placeholder,
+  value,
+}: TextInputErrorDisabledProps) {
+  const inputClass = `${styles.input} ${errorMessage ? styles.error : ''} ${
+    isDisabled ? styles.disabled : ''
+  } ${styles[variant]}`; // Usa variant para modificar la clase del input
+
+  return (
+    <div className={styles.inputWrapper}>
+      <span className={styles.inputContainer}>
+        <input
+          className={inputClass}
+          disabled={isDisabled}
+          placeholder={placeholder}
+          value={value}
+        />
+        {errorMessage && <IoAlertCircle className={styles.icon} />}
+      </span>
+      {errorMessage && !isDisabled && <p className={styles.errorMessage}>{errorMessage}</p>}
+    </div>
+  );
+}
+
+export default TextInputErrorDisabled;
