8da8e9c5002897e39b6fc9c672740ca9c1071dd8
Beymar <patriciobeymar4@gmail.com>
2024-11-25 21:20:23 -0400

    feature(common)/SEM-72

---
+++ src/ui/components/molecules/TopElements/UnifiedButtonProps.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import PrimaryButton from '../../atoms/Primary-Button/PrimaryButton';
+import SecondaryButton from '../../atoms/SecondaryButton/SecondaryButton';
+import DangerButton from '../../atoms/DangerButton/Danger-Button';
+
+interface UnifiedButtonProps {
+  variant: 'primary' | 'secondary' | 'tertiary';
+  children: React.ReactNode;
+  onClick?: () => void;
+  icon?: string;
+  display?: string;
+}
+
+function UnifiedButton({ variant, children, onClick, icon, display }: UnifiedButtonProps) {
+  switch (variant) {
+    case 'primary':
+      return (
+        <PrimaryButton onClick={onClick} icon={icon} display={display} variant="Medium">
+          {children}
+        </PrimaryButton>
+      );
+    case 'secondary':
+      return (
+        <SecondaryButton onClick={onClick} display={display ?? 'defaultDisplay'} variant="medium">
+          {children}
+        </SecondaryButton>
+      );
+    case 'tertiary':
+      return (
+        <DangerButton onClick={onClick} variant="Normal" disabled={false}>
+          {children}
+        </DangerButton>
+      );
+    default:
+      return null;
+  }
+}
+
+UnifiedButton.defaultProps = {
+  onClick: () => {},
+  icon: '',
+  display: 'defaultDisplay',
+};
+
+export default UnifiedButton;
