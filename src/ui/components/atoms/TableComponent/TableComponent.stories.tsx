d3553dacb522b7cec73d4d0ab8574c9580d00f03
nnina32 <noeminina18@gmail.com>
2024-11-29 15:18:16 -0400

    fix: Table component and related styles

--- src/ui/components/atoms/TableComponent/TableComponent.stories.tsx
+++ src/ui/components/atoms/TableComponent/TableComponent.stories.tsx
@@ -14,8 +14,8 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     data: [
-        { id: 1, title: 'Test Title 1', visible: true, protected: false, updated: '2024-11-27' },
-        { id: 2, title: 'Test Title 2', visible: false, protected: true, updated: '2024-11-28' },
+      { id: 1, title: 'Test Title 1', visible: true, protected: false, updated: '2024-11-27' },
+      { id: 2, title: 'Test Title 2', visible: false, protected: true, updated: '2024-11-28' },
     ],
   },
 };
