ab39c8773c8efefcae570b32ac72bc9f760bde5b
cinthiaastoraiqueh <cinthiaastoraique2@gmail.com>
2024-12-01 00:13:16 -0400

    FORM FIELDS (VOLUME MANAGEMENT)

---
+++ src/ui/components/atoms/GenericInput/types/IProps.ts
@@ -0,0 +1,10 @@
+import type { ChangeEvent } from 'react';
+
+export interface IProps {
+  label?: string;
+  placeholder?: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  icon?: string;
+  onIconClick?: () => void;
+  options?: string[];
+}
