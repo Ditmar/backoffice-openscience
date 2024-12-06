f8ef6996185b4e44aad269549c62e49a2ab1c9b5
ccordleyy <68717425+ccordleyy@users.noreply.github.com>
2024-12-06 18:15:00 -0400

    feat: sem-74 added bottom elements of new entry (bibliographies) (#257)
    * [SEM-74]: added bottom elements of new entry
    
    *  Trigger Build
    
    * [SEM-74]: adding mixins

---
+++ src/ui/components/molecules/ButtonNewEntryBibliographies/types/IProps.ts
@@ -0,0 +1,8 @@
+import type { ChangeEvent } from 'react';
+
+export interface IProps {
+  ids?: string;
+  label?: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  size?: 'small' | 'medium' | 'large';
+}
