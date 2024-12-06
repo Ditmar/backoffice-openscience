a669c9f1576358ca6b56684e6f25d211bf10fc23
Marco <narakucoronel@gmail.com>
2024-12-06 03:01:56 -0400

    initel

--- src/ui/components/atoms/Primarybuton/Primary-Button.stories.tsx
+++ src/ui/components/atoms/Primarybuton/Primary-Button.stories.tsx
@@ -1,5 +1,5 @@
 import type { StoryObj, Meta } from '@storybook/react';
-import DangerButton from './Primary-Button';
+import PrimaryButton from './Primary-Button';
 
 const meta = {
   title: 'ui/components/atoms/Primary-Button',
