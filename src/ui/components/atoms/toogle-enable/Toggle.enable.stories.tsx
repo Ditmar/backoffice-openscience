465480936015311925fa39003a777d29afe0be80
Marco <narakucoronel@gmail.com>
2024-12-06 23:55:59 -0400

    rl

--- src/ui/components/atoms/toogle-enable/Toggle.enable.stories.tsx
+++ src/ui/components/atoms/toogle-enable/Toggle.enable.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import Toggle from './Toggle.enable';
 
 const meta: Meta<typeof Toggle> = {
-  title: 'ui/components/atoms/toggle-enable',
+  title: 'ui/components/atoms/toggle.enable',
   component: Toggle,
   argTypes: {
     label: { control: 'text' },
