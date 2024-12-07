d88abc0f79b3e65a62fc79ea48533f9942504cb9
user.name <email@gmail.com>
2024-12-07 00:49:40 -0400

    branch pull

--- src/ui/components/atoms/TextInputErrorDisabled/types/IProps.ts
+++
@@ -1,8 +0,0 @@
-export interface TextInputErrorDisabledProps {
-  variant?: 'small' | 'medium' | 'large';
-  errorMessage?: string;
-  isDisabled?: boolean; 
-  placeholder?: string; 
-  value?: string;
-  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void; // Función para manejar cambios
-}
